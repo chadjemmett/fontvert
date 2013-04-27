e980f23c60fd8e94281f365dc146a86b0cf77fb5
Chad Jemmett <chad.jemmett@gmail.com>
2013-04-26 19:59:00 -0600

    first commit. Added an initial route to target.  controller needs to match what's in routes.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fontvert::Application.config.secret_token = 'c615001b1aa9fc23948b9cb2fdf3d94267c5396876396bbcd4b5cfab0ffb570691ab57742d13120d48d6814dd8061a9af96614617140240712aed202f11b2687'
